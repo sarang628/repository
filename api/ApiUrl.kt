53ef86c5e4d2d3feb7f8543216e5fa859f2a259b
SRYANG <sarang628@naver.com>
2023-12-18 14:54:10 +0800

    update login

--- api/ApiUrl.kt
+++ api/ApiUrl.kt
@@ -2,7 +2,7 @@ package com.sryang.torang_repository.di.repository.api
 
 object ApiUrl
 {
-//    val local = "http://192.168.0.14:8081/"
-    val local = "http://192.168.1.10:8081/"
+    val local = "http://192.168.0.14:8081/"
+//    val local = "http://192.168.1.10:8081/"
     val prod = "http://sarang628.iptime.org:8081/"
 }
