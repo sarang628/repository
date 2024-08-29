4ecd6b9bc7d3c8daaed5245fdc2c671412842cca
sryang <sarang628@gmail.com>
2024-08-29 18:31:48 +0800

    update

--- api/ApiCommentModule.kt
+++ api/ApiCommentModule.kt
@@ -30,7 +30,7 @@ class ProductApiComment @Inject constructor(
     private val torangOkHttpClientImpl: TorangOkhttpClient,
     private val retrofitModule: RetrofitModule
 ) {
-    private var url = "http://sarang628.iptime.org:8081/"
+    private var url = ApiUrl.comment
     fun create(): ApiComment {
         return retrofitModule
 //            .getRetrofit(torangOkHttpClientImpl.getUnsafeOkHttpClient(), url)
@@ -38,20 +38,3 @@ class ProductApiComment @Inject constructor(
             .create(ApiComment::class.java)
     }
 }
-
-/**
- * 로컬 서버 피드 서비스
- */
-@Singleton
-class LocalApiComment @Inject constructor(
-    private val torangOkHttpClientImpl: TorangOkhttpClient,
-    private val retrofitModule: RetrofitModule
-) {
-    private var url = "http://172.20.10.2:8081/"
-    fun create(): ApiComment {
-        return retrofitModule.getRetrofit(torangOkHttpClientImpl.getHttpClient(), url).create(
-            ApiComment::class.java
-        )
-    }
-}
-
