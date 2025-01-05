43d3325c94881eff9d8545574c7bbaa954e5cec7
sryang <sarang628@gmail.com>
2025-01-05 16:43:21 +0900

    update repository module version

--- api/ApiUrl.kt
+++ api/ApiUrl.kt
@@ -3,7 +3,8 @@ package com.sarang.torang.di.repository.api
 object ApiUrl {
     //    val local = "http://192.168.55.100:8081/"
     val coffeeBean = "http://172.16.101.226:8081/"
-    val local = coffeeBean
+    val starbucks = "http://172.30.42.212:8081/"
+    val local = starbucks
 
     //    val local = "http://192.168.1.10:8081/"
     val prod = "http://sarang628.iptime.org:8081/"
