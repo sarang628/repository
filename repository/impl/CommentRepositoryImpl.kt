416951789ac34703136def7c0e0ae5bba8a6a300
sryang <sarang628@gmail.com>
2025-04-06 12:35:00 +0900

    fix comment sending error

--- repository/impl/CommentRepositoryImpl.kt
+++ repository/impl/CommentRepositoryImpl.kt
@@ -86,30 +86,26 @@ class CommentRepositoryImpl @Inject constructor(
 
     override suspend fun addComment(reviewId: Int, comment: String, onLocalUpdated: () -> Unit) {
         //로그인 토큰 가져오기
-        sessionClientService.getToken().let {
-            val user = loggedInUserDao.getLoggedInUser1()
-            if (it == null || user == null) { // 토큰이 없거나 로그인한 사용자 정보가 없다면
-                throw Exception("로그인 해주세요")
-            }
-            val commentEntity = CommentEntity(
-                userName = user.userName,
-                comment = comment,
-                reviewId = reviewId,
-                userId = user.userId,
-                profilePicUrl = loggedInUserDao.getLoggedInUser1()?.profilePicUrl ?: "",
-                parentCommentId = 0,
-                isUploading = true
-            )
-            Log.d(
-                "__CommentRepositoryImpl",
-                SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis())
-            )
-            commentDao.insertComment(commentEntity)
-            onLocalUpdated.invoke()
-            delay(1000)
-            val result = apiComment.addComment(it, reviewId, comment)
-            commentDao.update(updateId = commentEntity.commentId, result.toCommentEntity())
+        val token = sessionClientService.getToken()
+        val user = loggedInUserDao.getLoggedInUser1()
+        if (token == null || user == null) { // 토큰이 없거나 로그인한 사용자 정보가 없다면
+            Log.e(TAG, "로그인 해주세요")
+            throw Exception("로그인 해주세요")
         }
+        val commentEntity = CommentEntity(
+            userName = user.userName,
+            comment = comment,
+            reviewId = reviewId,
+            userId = user.userId,
+            profilePicUrl = loggedInUserDao.getLoggedInUser1()?.profilePicUrl ?: "",
+            parentCommentId = 0,
+            isUploading = true
+        )
+        commentDao.insertComment(commentEntity)
+        onLocalUpdated.invoke()
+        delay(1000)
+        val result = apiComment.addComment(token, reviewId, comment)
+        commentDao.update(updateId = commentEntity.commentId, result.toCommentEntity())
     }
 
     override suspend fun addReply(
