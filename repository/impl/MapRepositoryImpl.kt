45bcc0dd5eb3bbd02918dee9f40d4a47f9cbb029
sryang <sarang628@gmail.com>
2025-08-31 10:31:18 +0800

    refactor : arrange code

--- repository/impl/MapRepositoryImpl.kt
+++ repository/impl/MapRepositoryImpl.kt
@@ -13,59 +13,18 @@ class MapRepositoryImpl @Inject constructor(
     private var NElon: Double = 0.0
     private var SWlat: Double = 0.0
     private var SWlon: Double = 0.0
-    override fun saveLat(lat: Double) {
-        torangPreference.saveLat(lat)
-    }
-
-    override fun savelon(lon: Double) {
-        torangPreference.saveLon(lon)
-    }
-
-    override fun saveZoom(level: Float) {
-        torangPreference.saveZoom(level)
-    }
-
-    override fun loadLat(): Double {
-        return torangPreference.loadLat()
-    }
-
-    override fun loadLon(): Double {
-        return torangPreference.loadLon()
-    }
-
-    override fun loadZoom(): Float {
-        return torangPreference.loadZoom()
-    }
-
-    override fun setNElat(latitude: Double) {
-        NElat = latitude
-    }
-
-    override fun setNElon(longitude: Double) {
-        NElon = longitude
-    }
-
-    override fun setSWlat(latitude: Double) {
-        SWlat = latitude
-    }
-
-    override fun setSWlon(longitude: Double) {
-        SWlon = longitude
-    }
-
-    override fun getNElat(): Double {
-        return NElat
-    }
-
-    override fun getNElon(): Double {
-        return NElon
-    }
-
-    override fun getSWlat(): Double {
-        return SWlat
-    }
-
-    override fun getSWlon(): Double {
-        return SWlon
-    }
+    override fun saveLat    (lat: Double)       { torangPreference.saveLat(lat) }
+    override fun savelon    (lon: Double)       { torangPreference.saveLon(lon) }
+    override fun saveZoom   (level: Float)      { torangPreference.saveZoom(level) }
+    override fun loadLat    (): Double          { return torangPreference.loadLat() }
+    override fun loadLon    (): Double          { return torangPreference.loadLon() }
+    override fun loadZoom   (): Float           { return torangPreference.loadZoom() }
+    override fun setNElat   (latitude: Double)  { NElat = latitude }
+    override fun setNElon   (longitude: Double) { NElon = longitude }
+    override fun setSWlat   (latitude: Double)  { SWlat = latitude }
+    override fun setSWlon   (longitude: Double) { SWlon = longitude }
+    override fun getNElat   (): Double          { return NElat }
+    override fun getNElon   (): Double          { return NElon }
+    override fun getSWlat   (): Double          { return SWlat }
+    override fun getSWlon   (): Double          { return SWlon }
 }
